01940ed39951bc8ba15f174770c4e1a50cd28ff4
DimaK <dimak@admin@pdd-vmg.ru>
2023-05-12 14:42:49 +0300

    добавил внешние ключи, что бы нельзя было подменять post_id и parent_id

--- database/migrations/2023_04_22_075057_create_comments_table.php
+++ database/migrations/2023_04_22_075057_create_comments_table.php
@@ -4,28 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
+
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->integer('post_id');
+            $table->unsignedBigInteger('post_id');
             $table->integer('user_id');
-            $table->integer('parent_id')->default(0);
+            $table->unsignedBigInteger('parent_id')->nullable();
             $table->text('text');
             $table->timestamps();
+            $table->foreign('post_id')
+                    ->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('parent_id')
+                    ->references('id')->on('comments')->onDelete('cascade');
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('comments');
     }
 };
